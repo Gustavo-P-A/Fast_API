5bb44938bcc2bd16b467d200b767e6c9f71b9cf0
Gustavo-P-A <gu1306.peres@gmail.com>
2026-02-17 22:07:32 -0300

    Fix: Reinstalação limpa do Pydantic para Python 3.13 e criptografia de senha

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
-from models import Usuario, db
-from sqlalchemy.orm import sessionmaker
+from fastapi import APIRouter, Depends
+from models import Usuario
+from dependencies import pegar_sessao
+from main import bcrypt_context
 
 auth_router = APIRouter(prefix='/auth', tags=['auth '])
 
@@ -9,13 +10,13 @@ async def home():
     return{'mensagem': 'voce acesso a rota de autenticacao'}
 
 @auth_router.post('/criar_usuario')
-async def criar_usuario(email: str, senha: str, nome: str):
-    Session = sessionmaker(bind=db)
-    session = Session()
+async def criar_usuario(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
     usuario = session.query(Usuario).filter(Usuario.email == email).first()
     if usuario:
-        pass
+        return {"mensagem": 'já existe um usuario com esse email'}
     else:
-        novo_usuario = Usuario(nome, email, senha)
+        senha_cripitografada = bcrypt_context.hash(senha)
+        novo_usuario = Usuario(nome, email, senha_cripitografada)
         session.add(novo_usuario)
-        
+        session.commit()
+        return {"mensagem": "usuário cadastrado com sucesso"}
