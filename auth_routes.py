25ad6c98351451084548aaac86dd47d613c092ea
Gustavo-P-A <gu1306.peres@gmail.com>
2026-02-17 21:20:31 -0300

    Criacao do Banco de Dados

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,21 @@
 from fastapi import APIRouter
+from models import Usuario, db
+from sqlalchemy.orm import sessionmaker
 
 auth_router = APIRouter(prefix='/auth', tags=['auth '])
 
 @auth_router.get('/')
-async def autenticar():
+async def home():
     return{'mensagem': 'voce acesso a rota de autenticacao'}
+
+@auth_router.post('/criar_usuario')
+async def criar_usuario(email: str, senha: str, nome: str):
+    Session = sessionmaker(bind=db)
+    session = Session()
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if usuario:
+        pass
+    else:
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        
---
+++ models.py
@@ -0,0 +1,66 @@
+from sqlalchemy import create_engine, Column, String, Integer, Float, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils import ChoiceType
+
+#conexao do banco de dados
+db = create_engine('sqlite:///banco.db')
+
+#criação do db
+Base = declarative_base()
+
+#criação das classes
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column('id', Integer, autoincrement=True ,primary_key=True)
+    nome = Column('nome', String, nullable=False)
+    email = Column('email', String, nullable=False, unique=True)
+    senha = Column('senha', String, nullable=False)
+    ativo = Column('ativo', Boolean)
+    adm = Column("adm", Boolean, default=False)
+
+    def __init__(self,nome, email, senha, ativo=True, adm=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.adm = adm
+
+class Pedidos(Base):
+    __tablename__ = 'pedidos'
+
+    # STATUS = (
+    #     ("PENDENTE", "PENDENTE"),
+    #     ("CANCELADO", "CANCELADO"),
+    #     ("FINALIZADO", "FINALIZADO")
+    # )
+
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    status = Column("status", String)
+    usuario = Column('usuario', ForeignKey('usuarios.id'))
+    preco = Column('preco', Float)
+    # itens =
+
+    def __init__(self,usuario, status='PENDENTE', preco=0):
+        self.status = status
+        self.preco = preco
+        self.usuario = usuario
+
+class ItemPedido(Base):
+    __tablename__ = 'itens_pedido'
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column('quantidade', Integer)
+    sabor = Column('sabor', String)
+    tamanho = Column('tamanho', String)
+    preco = Column('preco', Float)
+    pedido = Column('pedido', ForeignKey('pedidos.id'))
+
+    def __init__(self, quantidade, sabor, tamanho, preco, pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.pedido = pedido
+        self.tamanho = tamanho
+        self.preco = preco
+
+#execução dos metadados 
