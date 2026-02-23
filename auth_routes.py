14cf527a635a602557e36b1478842cde57db5d16
Gustavo-P-A <gu1306.peres@gmail.com>
2026-02-23 17:53:29 -0300

    Atualizacoes nas rotas

--- auth_routes.py
+++ auth_routes.py
@@ -1,17 +1,24 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pegar_sessao
-from main import bcrypt_context
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 auth_router = APIRouter(prefix='/auth', tags=['auth '])
 
 
-def criar_token(id_usuario):
-    token = f'ondwgfn98wn9gn{id_usuario}'
-    return token
+def criar_token(id_usuario, duracao_token=ACCESS_TOKEN_EXPIRE_MINUTES):
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dic_info = {'sub': id_usuario, 'exp': data_expiracao}
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_codificado
 
+def verificar_token(token, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter_by(Usuario.id==1).first()
+    return usuario
 
 def autenticar_usuario(email, senha, session):
     usuario = session.query(Usuario).filter(Usuario.email == email).first()
@@ -46,4 +53,11 @@ async def login(login_schema: LoginSchema, session: Session =  Depends(pegar_ses
         raise HTTPException(status_code=400, detail='Usuario não encontrado ou credenciais invalidas')
     else:
         access_token = criar_token(usuario.id)
-        return {"access_token": access_token, 'token_type': 'Bearer'}
+        refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
+        return {"access_token": access_token, 'refresh_token': refresh_token ,'token_type': 'Bearer'}
+    
+@auth_router.get('/refresh')
+async def use_refresh_token(token):
+    usuario = verificar_token(token)
+    access_token = criar_token(usuario.id)
+    return {"access_token": access_token, 'token_type': 'Bearer'}
